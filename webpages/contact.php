6fac1d04d9bf711e56dc103fbf0f22b72796719e
rjamorim <>
2009-11-03 16:11:01 +0000

    Fixed slight bug related to quirks mode where scrollbars wouldn't show in IE8. Now the page looks like shit on IE5 and 5.5, but users of these browsers should suffer like the brats they are.

--- webpages/contact.php
+++ webpages/contact.php
@@ -9,7 +9,7 @@
 	<meta name="cvs-version" content="$Id$" />
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="styles/lame.css" />
-	<!--[if IE]>
+	<!--[if IE 6]>
 	<link rel="stylesheet" type="text/css" href="styles/ie.css" />
 	<![endif]-->
 </head>
